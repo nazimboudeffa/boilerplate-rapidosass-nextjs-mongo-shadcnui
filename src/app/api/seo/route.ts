99e678e998d96d3ba0dce2ff9fbc2019ed9a0f81
Nazim Boudeffa <boudeffa@gmail.com>
2024-03-30 22:27:29 +0100

    Removed file

--- src/app/api/seo/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server';
-
-export async function POST (request: NextRequest) {
-    const body = await request.json();
-    const res = await fetch(body.url)
-    const html = await res.text()
-   
-    return NextResponse.json({ data: html }, { status: 200 });
-}
